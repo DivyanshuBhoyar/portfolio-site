289362138ab0d8c7df8df1e885ca01ae9bf25c36
DivyanshuBhoyar <divyanshubhoyar@yahoo.com>
2022-07-31 20:24:21 +0530

    feat: tailwind grad-updated

--- src/partials/TechInfo.tsx
+++ src/partials/TechInfo.tsx
@@ -9,114 +9,66 @@ const TechContent = () => {
       <ul>
         <li className="py-2">
           <h3 className="text-xl font-medium">Backend</h3>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 ">
-            fastAPI
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            express
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            fiber (Go)
-          </div>
-          <div className="tech-item  bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            websockets
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            microservices
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            graphQL
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            apollo-server
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            tRPC
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            gRPC
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            jwt
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            event-bus
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            fastify-js
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            REST
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            aws-lambda
-          </div>
-          <div className="tech-item bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text ">
-            stripe
-          </div>
+          <div className="tech-item grad0 ">fastAPI</div>
+          <div className="tech-item grad0  ">express</div>
+          <div className="tech-item grad0  ">fiber (Go)</div>
+          <div className="tech-item  grad0  ">websockets</div>
+          <div className="tech-item grad0  ">microservices</div>
+          <div className="tech-item grad0  ">graphQL</div>
+          <div className="tech-item grad0  ">apollo-server</div>
+          <div className="tech-item grad0  ">tRPC</div>
+          <div className="tech-item grad0  ">gRPC</div>
+          <div className="tech-item grad0  ">jwt</div>
+          <div className="tech-item grad0  ">event-bus</div>
+          <div className="tech-item grad0  ">fastify-js</div>
+          <div className="tech-item grad0  ">REST</div>
+          <div className="tech-item grad0  ">aws-lambda</div>
+          <div className="tech-item grad0  ">stripe</div>
         </li>
         <li className="py-2">
           <h3 className="text-xl font-medium">Databases/ ORM</h3>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            mongo-db
-          </div>
-          <div className="tech-item  bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            postgres
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            redis
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            firestore
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            mysql
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            mongoose (ORM)
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            typeorm (ORM)
-          </div>
-          <div className="tech-item bg-gradient-to-tl from-pink-300 via-purple-300 to-indigo-400 bg-clip-text px-1 font-bold text-transparent ">
-            prisma (ORM)
-          </div>
+          <div className="tech-item grad3 ">mongo-db</div>
+          <div className="tech-item  grad3 ">postgres</div>
+          <div className="tech-item grad3 ">redis</div>
+          <div className="tech-item grad3 ">firestore</div>
+          <div className="tech-item grad3 px-1  ">mysql</div>
+          <div className="tech-item grad3 ">mongoose (ORM)</div>
+          <div className="tech-item grad3 ">typeorm (ORM)</div>
+          <div className="tech-item grad3 ">prisma (ORM)</div>
         </li>
         <li className="py-2">
           <h3 className="text-xl font-medium">Cloud/Devops</h3>
-          <div className="tech-item  bg-green-700 px-1 ">aws</div>
-          <div className="tech-item  bg-green-700 px-1 ">cloudflare</div>
-          <div className="tech-item  bg-green-700 px-1 ">firebase</div>
-          <div className="tech-item  bg-green-700 px-1 ">
-            serverless-framework
-          </div>
-          <div className="tech-item  bg-green-700 px-1 ">docker</div>
-          <div className="tech-item  bg-green-700 px-1 ">Kubernetes</div>
-          <div className="tech-item  bg-green-700 px-1 ">vercel</div>
-          <div className="tech-item  bg-green-700 px-1 ">digitalocean</div>
-          <div className="tech-item  bg-green-700 px-1 ">github-actions</div>
+          <div className="tech-item  grad2 ">aws</div>
+          <div className="tech-item  grad2 ">cloudflare</div>
+          <div className="tech-item  grad2 ">firebase</div>
+          <div className="tech-item  grad2 ">serverless-framework</div>
+          <div className="tech-item  grad2 ">docker</div>
+          <div className="tech-item  grad2 ">Kubernetes</div>
+          <div className="tech-item  grad2 ">vercel</div>
+          <div className="tech-item  grad2 ">digitalocean</div>
+          <div className="tech-item  grad2 ">github-actions</div>
         </li>
         <li className="py-2">
           <h3 className="text-xl font-medium">Frontend</h3>
-          <div className="tech-item  bg-green-700 px-1 ">next-js</div>
-          <div className="tech-item  bg-green-700 px-1 ">react-query</div>
-          <div className="tech-item  bg-green-700 px-1 ">angular</div>
-          <div className="tech-item  bg-green-700 px-1 ">tailwind-css</div>
-          <div className="tech-item  bg-green-700 px-1 ">styled-components</div>
-          <div className="tech-item  bg-green-700 px-1 ">material-ui</div>
-          <div className="tech-item  bg-green-700 px-1 ">mantine-ui</div>
-          <div className="tech-item  bg-green-700 px-1 ">sass</div>
-          <div className="tech-item  bg-green-700 px-1 ">framer-motion</div>
-          <div className="tech-item  bg-green-700 px-1 ">astro-js</div>
-          <div className="tech-item  bg-green-700 px-1 ">flutter</div>
+          <div className="tech-item grad1  ">next-js</div>
+          <div className="tech-item grad1  ">react-query</div>
+          <div className="tech-item grad1  ">angular</div>
+          <div className="tech-item grad1  ">tailwind-css</div>
+          <div className="tech-item grad1  ">styled-components</div>
+          <div className="tech-item grad1  ">material-ui</div>
+          <div className="tech-item grad1  ">mantine-ui</div>
+          <div className="tech-item grad1  ">sass</div>
+          <div className="tech-item grad1  ">framer-motion</div>
+          <div className="tech-item grad1  ">astro-js</div>
+          <div className="tech-item grad1  ">flutter</div>
         </li>
         <li className="py-2">
           <h3 className="text-xl font-medium">Other tools</h3>
-          <div className="tech-item  bg-green-700 px-1 ">jest</div>
-          <div className="tech-item  bg-green-700 px-1 ">cypress</div>
-          <div className="tech-item  bg-green-700 px-1 ">postman</div>
-          <div className="tech-item  bg-green-700 px-1 ">git</div>
-          <div className="tech-item  bg-green-700 px-1 ">figma</div>
+          <div className="tech-item  grad4 ">jest</div>
+          <div className="tech-item  grad4 ">cypress</div>
+          <div className="tech-item  grad4 ">postman</div>
+          <div className="tech-item  grad4 ">git</div>
+          <div className="tech-item  grad4 ">figma</div>
         </li>
       </ul>
     </>
